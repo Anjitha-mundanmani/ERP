88bd8b7850b52c473d7fdf81de7b5af7149e91ec
Anjitha <184491@ust.com>
2021-08-18 12:22:04 +0530

    erp using class

---
+++ class_erp/search.py
@@ -0,0 +1,58 @@
+from emp_store import employees
+
+def search_menu():
+	print("1. Search by name")
+	print("2. Search by age")
+	print("3. Search by salary")
+	print("4. Search by gender")
+	print("5. exit")
+	
+def search_main():	
+	while True:
+		search_menu()
+		cho = int(input("Enter your choice"))
+		if cho == 1:
+			name = input("Enter name :")
+			st = list(filter(lambda a: a.name == name , employees))
+			if len(st) == 0: #not st #st
+				print("No employee found")
+			else:
+				for i in st:
+					print(f"{i.name}")
+		elif cho == 2:
+			age = input("Enter age :")
+			st = list(filter(lambda a: a.age == age , employees))
+			if len(st) == 0: #not st #st
+				print("No employee found")
+			else:
+				for i in st:
+					print(f"{i.age}")
+					print(employees)
+		elif cho == 3:
+			salary = input("Enter salary :")
+			st = list(filter(lambda a: a.salary == salary , employees))
+			if len(st) == 0: #not st #st
+				print("No employee found")
+			else:
+				for i in st:
+					print(f"{i.salary}")
+					print(employees)
+		elif cho == 4:
+			gender = input("Enter gender :")
+			st = list(filter(lambda a: a.gender == gender , employees))
+			if len(st) == 0: #not st #st
+				print("No employee found")
+			else:
+				for i in st:
+					print(f"{i.gender}")
+					print(employees)
+		elif cho == 5:
+			break
+		else:
+			print("Invalid choice")
+
+
+
+
+
+
